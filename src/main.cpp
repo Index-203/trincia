31214b08eae03be559a5f0fee29df84cc080e0c6
Index-203 <gusifranz@gmail.com>
2023-04-03 23:40:42 +0200

    inizializzazione servo e pistoni

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
+#include <Servo.h>
+#include <RF24.h>
+
+#define ptaglio 13
+#define psblocco 14
+
+//definizione servo
+Servo acceleratore;
+Servo freno;
+Servo cambio_sel;
+Servo cambio_inn;
+Servo sblocc_dx;
+Servo sblocc_sx;
+Servo trincia_inn;
+Servo freno_trincia;
 
 void setup() {
-  // put your setup code here, to run once:
+  //servo inizializzazione
+  acceleratore.attach(9); 
+  freno.attach(9);
+  cambio_sel.attach(9);
+  cambio_inn.attach(9);
+  sblocc_dx.attach(9);
+  sblocc_sx.attach(9);
+  trincia_inn.attach(9);
+  freno_trincia.attach(9); 
+
+  //inizializzazione pistoni elettrici
+  pinMode(ptaglio, OUTPUT);
+  pinMode(psblocco, OUTPUT);
+
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
 }
