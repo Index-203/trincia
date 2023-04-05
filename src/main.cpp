283a22ea80b190922476cc56b505a4be4e3124d6
iTTou <perotti.denis@gmail.com>
2023-04-05 13:07:55 +0200

    translate to eng

--- src/main.cpp
+++ src/main.cpp
@@ -3,40 +3,41 @@
 #include <RF24.h>
 #include <Arduino_FreeRTOS.h>
 
-#define ptaglio 40
-#define psblocco 42
+#define pCut 40
+#define pUnlock 42
 
-//definizione servo
-Servo acceleratore;
-Servo freno;
-Servo cambio_sel;
-Servo cambio_inn;
-Servo sblocc_dx;
-Servo sblocc_sx;
-Servo trincia_inn;
-Servo freno_trincia;
+//servos
+Servo throttle;
+Servo brakes;
+Servo shifter_sel;
+Servo shifter_eng;
+Servo unlock_right;
+Servo unlock_left;
+Servo trincia_eng;
+Servo trincia_brakes;
 
 void setup() {
-  //servo inizializzazione
-  acceleratore.attach(22); 
-  freno.attach(24);
-  cambio_sel.attach(28);
-  cambio_inn.attach(30);
-  sblocc_dx.attach(32);
-  sblocc_sx.attach(34);
-  trincia_inn.attach(36);
-  freno_trincia.attach(38); 
 
-  //inizializzazione pistoni elettrici
-  pinMode(ptaglio, OUTPUT);
-  pinMode(psblocco, OUTPUT);
+    //servo initialization
+    throttle.attach(22);
+    brakes.attach(24);
+    shifter_sel.attach(28);
+    shifter_eng.attach(30);
+    unlock_right.attach(32);
+    unlock_left.attach(34);
+    trincia_eng.attach(36);
+    trincia_brakes.attach(38);
+
+    //electric pistons initialization
+    pinMode(pCut, OUTPUT);
+    pinMode(pUnlock, OUTPUT);
 
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(1000);                      // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    delay(1000);                      // wait for a second
 }
