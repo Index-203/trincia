b90648429d24da63c075d5fa7f96879b4cdf91d1
Index-203 <gusifranz@gmail.com>
2023-04-06 00:26:13 +0200

    define how servo works

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,11 @@
 #include <RF24.h>
 #include <Arduino_FreeRTOS.h>
 
-#define pCut 40
-#define pUnlock 42
+#define pCut 40 //piston height cut
+#define pCut1 41 //piston height cut
+#define pUnlock 42 //piston lock
+#define pUnlock1 43 //piston lock
+#define safe_stop 43 //safety stop
 
 //servos
 Servo throttle;
@@ -13,24 +16,27 @@ Servo shifter_sel;
 Servo shifter_eng;
 Servo unlock_right;
 Servo unlock_left;
-Servo trincia_eng;
-Servo trincia_brakes;
+Servo rotor_mower_eng;
+Servo rotor_brakes;
 
 void setup() {
 
     //servo initialization
-    throttle.attach(22);
-    brakes.attach(24);
-    shifter_sel.attach(28);
-    shifter_eng.attach(30);
-    unlock_right.attach(32);
-    unlock_left.attach(34);
-    trincia_eng.attach(36);
-    trincia_brakes.attach(38);
+    throttle.attach(22);//var 0 180                 //potentiometer
+    brakes.attach(24);//0 180                       //by software
+    shifter_sel.attach(28);//var 0 180  r 0 1 2 3   //recive r 0 1 2 3
+    shifter_eng.attach(30);//0 180                  //by software & recive
+    unlock_right.attach(32);//var 0 180 3 pos       //recive 3 valor pos 1 pos 2 pos 3
+    unlock_left.attach(34);//var 0 180  3 pos       //recive 3 valor pos 1 pos 2 pos 3
+    rotor_mower_eng.attach(36);// 0 180             //recive a boolean valor 0= disengage 1=engage
+    rotor_brakes.attach(38);//optional
 
     //electric pistons initialization
     pinMode(pCut, OUTPUT);
+    pinMode(pCut1, OUTPUT);
     pinMode(pUnlock, OUTPUT);
+    pinMode(pUnlock1, OUTPUT);
+    pinMode(safe_stop, OUTPUT);
 
 }
 
